67ad4664983f3ee644ea118940731e301cee004a
jeroidhelp46 <jeroidhelp469@gmail.com>
2025-05-18 09:29:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "jeroidhelp469@gmail.com";
-const pass = "ufiutkwtwdhntbey";
+//const pass = "ufiutkwtwdhntbey";
 // 15 june
 
 // Middleware
